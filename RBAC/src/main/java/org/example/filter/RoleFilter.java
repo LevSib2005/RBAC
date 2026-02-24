f45457fd743102ad1e19d89e1108ac656a7d9090
LevSib2005 <kengymsen@gmail.com>
2026-02-24 20:56:45 +0700

    2.2. Функциональный интерфейс для фильтрации ролей

---
+++ RBAC/src/main/java/org/example/filter/RoleFilter.java
@@ -0,0 +1,16 @@
+package org.example.filter;
+
+import org.example.entity.Role;
+
+@FunctionalInterface
+public interface RoleFilter {
+    boolean test(Role role);
+
+    default RoleFilter and(RoleFilter other) {
+        return role -> this.test(role) && other.test(role);
+    }
+
+    default RoleFilter or(RoleFilter other) {
+        return role -> this.test(role) || other.test(role);
+    }
+}
